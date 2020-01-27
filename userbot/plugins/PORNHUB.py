70eeb35ffcd039939f84c4eb15fd296d3ab3f342
venom_vipers <58991566+venomvipers@users.noreply.github.com>
2020-01-27 19:53:28 +0530

    Update PORNHUB.py

--- userbot/plugins/PORNHUB.py
+++ userbot/plugins/PORNHUB.py
@@ -166,3 +166,45 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 10])
+            
+            
+            @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.5
+
+    animation_ttl = range(0, 101)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "venom":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "V_",
+
+            "VE_",
+
+            "VEN_",
+
+            "VENO_",
+
+
+           
+            "VENOM",
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 10])
+
