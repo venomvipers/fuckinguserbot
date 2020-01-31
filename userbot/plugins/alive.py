6af5017158acf983204a9b388e2d352a0e19bef5
venom <58991566+venomvipers@users.noreply.github.com>
2020-01-31 05:40:18 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI (Maa Chod Denge Sbki) ψ(｀∇´)ψ`**\n\n"
+    await alive.edit("**`Suru majburi mein kiye the…..Ab maza aa rahe hain (Maa Chod Denge Sbki) ψ(｀∇´)ψ`**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @venom_vipers\n"
-                     "`Bot created by:` [SnapDragon](tg://user?id=1048670849)\n"
+                     "`Bot created by:` [乙Ŧ ツ Venom🕷️🕸️](tg://user?id=1048670849)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      f"`My Ultra Peru Owner`: {DEFAULTUSER}\n"
                      "[Join Kr Bkl Abhi Tk Nhi Kiya H Toh](https://t.me/hackershubbb)")
